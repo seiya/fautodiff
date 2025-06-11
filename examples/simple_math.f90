7e2b1855232676553908a287337a4db5bc09f3c1
Seiya Nishizawa <seiya@gfd-dennou.org>
2025-06-11 13:37:12 +0900

    Add subtraction, division and power examples

--- examples/simple_math.f90
+++ examples/simple_math.f90
@@ -25,4 +25,32 @@ contains
     return
   end subroutine multiply_numbers
 
+  function subtract_numbers(a, b) result(c)
+    real, intent(in) :: a, b
+    real :: c
+
+    c = a - b
+
+    return
+  end function subtract_numbers
+
+  subroutine divide_numbers(a, b, c)
+    real, intent(in) :: a, b
+    real, intent(out) :: c
+
+    c = a / b
+    c = c / 2.0 + a
+
+    return
+  end subroutine divide_numbers
+
+  function power_numbers(a, b) result(c)
+    real, intent(in) :: a, b
+    real :: c
+
+    c = a ** b
+
+    return
+  end function power_numbers
+
 end module simple_math
