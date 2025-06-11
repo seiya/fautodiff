55c46b1ccd8e9daef144142bbf05a249d9c83e02
Seiya Nishizawa <seiya@gfd-dennou.org>
2025-06-11 13:57:24 +0900

    update the sample fortran code

--- examples/simple_math.f90
+++ examples/simple_math.f90
@@ -15,30 +15,31 @@ contains
     return
   end function add_numbers
 
-  subroutine multiply_numbers(a, b, c)
-    real, intent(in) :: a, b
-    real, intent(out) :: c
-
-    c = a * b
-    c = c * 3.0 + a
-
-    return
-  end subroutine multiply_numbers
-
   function subtract_numbers(a, b) result(c)
     real, intent(in) :: a, b
     real :: c
 
     c = a - b
+    c = - c + b
 
     return
   end function subtract_numbers
+
+  subroutine multiply_numbers(a, b, c)
+    real, intent(in) :: a, b
+    real, intent(out) :: c
+
+    c = a * b + a
+    c = c * 3.0 + a
+
+    return
+  end subroutine multiply_numbers
 
   subroutine divide_numbers(a, b, c)
     real, intent(in) :: a, b
     real, intent(out) :: c
 
-    c = a / b
+    c = a / (b + 1.5)
     c = c / 2.0 + a
 
     return
@@ -48,7 +49,8 @@ contains
     real, intent(in) :: a, b
     real :: c
 
-    c = a ** b
+    c = a**3 + b**5.5
+    c = c + a**b + (4.0 * a + 2.0)**b + a**(b * 5.0 + 3.0)
 
     return
   end function power_numbers
--- examples/simple_math_ad.f90
+++ examples/simple_math_ad.f90
@@ -31,6 +31,29 @@ contains
     return
   end subroutine add_numbers_ad
 
+  subroutine subtract_numbers_ad(a, a_ad, b, b_ad, c_ad)
+    real, intent(in)  :: a
+    real, intent(out) :: a_ad
+    real, intent(in)  :: b
+    real, intent(out) :: b_ad
+    real, intent(in)  :: c_ad
+    real :: dc_dc
+    real :: dc_db
+    real :: c_ad_
+    real :: dc_da
+
+    dc_dc = - 1.0
+    dc_db = 1.0
+    b_ad = c_ad * dc_db
+    c_ad_ = c_ad * dc_dc
+    dc_da = 1.0
+    dc_db = - 1.0
+    b_ad = c_ad_ * dc_db
+    a_ad = c_ad_ * dc_da
+
+    return
+  end subroutine subtract_numbers_ad
+
   subroutine multiply_numbers_ad(a, a_ad, b, b_ad, c_ad)
     real, intent(in)  :: a
     real, intent(out) :: a_ad
@@ -46,7 +69,7 @@ contains
     dc_da = 1.0
     a_ad = c_ad * dc_da
     c_ad_ = c_ad * dc_dc
-    dc_da = b
+    dc_da = b + 1.0
     dc_db = a
     b_ad = c_ad_ * dc_db
     a_ad = c_ad_ * dc_da + a_ad
@@ -54,24 +77,25 @@ contains
     return
   end subroutine multiply_numbers_ad
 
-  subroutine subtract_numbers_ad(a, a_ad, b, b_ad, c_ad)
-    real, intent(in)  :: a
-    real, intent(out) :: a_ad
-    real, intent(in)  :: b
-    real, intent(out) :: b_ad
-    real, intent(in)  :: c_ad
-
-
-    return
-  end subroutine subtract_numbers_ad
-
   subroutine divide_numbers_ad(a, a_ad, b, b_ad, c_ad)
     real, intent(in)  :: a
     real, intent(out) :: a_ad
     real, intent(in)  :: b
     real, intent(out) :: b_ad
     real, intent(in)  :: c_ad
+    real :: dc_dc
+    real :: dc_da
+    real :: c_ad_
+    real :: dc_db
 
+    dc_dc = 1.0 / 2.0
+    dc_da = 1.0
+    a_ad = c_ad * dc_da
+    c_ad_ = c_ad * dc_dc
+    dc_da = 1.0 / (b + 1.5)
+    dc_db = - a / (b + 1.5)**2
+    b_ad = c_ad_ * dc_db
+    a_ad = c_ad_ * dc_da + a_ad
 
     return
   end subroutine divide_numbers_ad
@@ -83,6 +107,16 @@ contains
     real, intent(out) :: b_ad
     real, intent(in)  :: c_ad
 
+    dc_dc = 1.0
+    dc_da = b * a**(b - 1.0) + b * (4.0 * a + 2.0)**(b - 1.0) * 4.0 + (b * 5.0 + 3.0) * a**(b * 5.0 + 2.0)
+    dc_db = a**b * log(a) + (4.0 * a + 2.0)**b * log(4.0 * a + 2.0) + a**(b + 5.0 + 3.0) * log(a) * 5.0
+    b_ad = c_ad * dc_db
+    a_ad = c_ad * dc_da
+    c_ad_ = c_ad * dc_dc
+    dc_da = 3 * a**2
+    dc_db = 5.5 * b**4.5
+    b_ad = c_ad_ * dc_db + b_ad
+    a_ad = c_ad_ * dc_da + a_ad
 
     return
   end subroutine power_numbers_ad
