a06aa382e3e67a5949ac91491adfc206c315d24e
Seiya Nishizawa <seiya@gfd-dennou.org>
2025-06-23 00:39:10 +0900

    array save var

--- examples/save_vars.f90
+++ examples/save_vars.f90
@@ -41,7 +41,7 @@ contains
     return
   end subroutine if_example
 
-  subroutine do_with_array(n, m, x, y, z)
+  subroutine do_with_array_private(n, m, x, y, z)
     integer, intent(in) :: n, m
     real, intent(in) :: x(n,m), y(n,m)
     real, intent(out) :: z(n,m)
@@ -59,6 +59,37 @@ contains
           z(i,j) = y(i,j) * scalar + z(i,j)
           scalar = z(i,j) * y(i,j)
           z(i,j) = z(i,j) * scalar
+       end do
+    end do
+
+    return
+  end subroutine do_with_array_private
+
+  subroutine do_with_array(n, m, x, y, z)
+    integer, intent(in) :: n, m
+    real, intent(in) :: x(n,m), y(n,m)
+    real, intent(out) :: z(n,m)
+    real :: ary(n,m)
+    integer :: i, j
+
+    do j = 1, m
+       do i = 1, n
+          ary(i,j) = x(i,j) + 1.0
+       end do
+    end do
+
+    do j = 1, m
+       do i = 1, n
+          z(i,j) = ary(i,j) * x(i,j)
+          ary(i,j) = ary(i,j) + z(i,j) * y(i,j)
+       end do
+    end do
+
+    do j = 1, m
+       do i = 1, n
+          z(i,j) = z(i,j) * x(i,j) + ary(i,j)
+          ary(i,j) = y(i,j) * ary(i,j)
+          z(i,j) = z(i,j) + ary(i,j)
        end do
     end do
 
