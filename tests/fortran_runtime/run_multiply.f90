ffe432bd5d584bb47d0844fc2cb91efc3436f7bc
Seiya Nishizawa <seiya@gfd-dennou.org>
2025-06-26 09:40:35 +0900

    Add Fortran runtime tests

---
+++ tests/fortran_runtime/run_multiply.f90
@@ -0,0 +1,17 @@
+program run_multiply
+  use simple_math
+  use simple_math_ad
+  real :: a, b, c
+  real :: a_ad, b_ad, c_ad
+
+  a = 2.0
+  b = 3.0
+  call multiply_numbers(a, b, c)
+
+  a_ad = 0.0
+  b_ad = 0.0
+  c_ad = 1.0
+  call multiply_numbers_ad(a, a_ad, b, b_ad, c_ad)
+
+  print *, c, a_ad, b_ad
+end program run_multiply
