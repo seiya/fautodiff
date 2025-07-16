3bb3c543a28725d48223a8195e188379c6ae5496
Seiya Nishizawa <seiya@gfd-dennou.org>
2025-07-16 12:08:30 +0900

    add label to do loop in parser and render

--- examples/exit_cycle.f90
+++ examples/exit_cycle.f90
@@ -65,4 +65,47 @@ end subroutine do_exit_cycle
     res = res * x
   end subroutine while_exit_cycle
 
+  subroutine exit_cycle_with_labels(n, x, res)
+    integer, intent(in) :: n
+    real, intent(in) :: x
+    real, intent(out) :: res
+    integer :: i
+    integer :: j
+    integer :: k
+
+    res = x
+    i = 0
+    outer: do while (i <= n)
+      i = i + 1
+      res = res + 1.0
+      middle: do j = 1, n
+        res = res + 10.0
+        if (res > 5000.0) then
+          exit outer
+        end if
+        inner: do k = 1, n
+          if (res > 4000.0) then
+            exit outer
+          end if
+          res = res + 100.0
+          if (res > 2400.0) then
+            exit middle
+          end if
+          res = res + 100.0
+          if (res > 2200.0) then
+            cycle middle
+          end if
+          res = res + 100.0
+          if (res > 1000.0) then
+            cycle outer
+          end if
+          res = res * x
+        end do inner
+      end do middle
+      res = res * x
+    end do outer
+
+    return
+  end subroutine exit_cycle_with_labels
+
 end module exit_cycle
